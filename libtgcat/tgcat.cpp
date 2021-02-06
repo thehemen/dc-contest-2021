45ba245fe7eae2cb93ef377db73e1cac7f67d79d
Vladimir Zhuravlev <hemen4815@gmail.com>
2021-02-06 21:36:27 +0700

    add language identification

--- libtgcat/tgcat.cpp
+++ libtgcat/tgcat.cpp
@@ -1,58 +1,44 @@
-#include "tgcat.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int tgcat_init() {
-  return 0;
+#include "tgcat.h"
+#include "utils.h"
+#include "lang_detect.h"
+
+#include "fasttext/fasttext.h"
+
+using namespace std;
+
+fasttext::FastText fastText;
+
+int tgcat_init()
+{
+	fastText.loadModel("../resources/lid.176.bin");
+    return 0;
 }
 
-int tgcat_detect_language(const struct TelegramChannelInfo *channel_info,
-                          char language_code[6]) {
-  // choose a text with the longest length
-  const char *text = channel_info->title;
-  size_t text_len = strlen(channel_info->title);
-  if (strlen(channel_info->description) > text_len) {
-    text = channel_info->description;
-    text_len = strlen(channel_info->description);
-  }
-  size_t i;
-  for (i = 0; i < channel_info->post_count; i++) {
-    if (strlen(channel_info->posts[i]) > text_len) {
-      text = channel_info->posts[i];
-      text_len = strlen(channel_info->posts[i]);
-    }
-  }
+int tgcat_detect_language(const struct TelegramChannelInfo *channel_info, char language_code[6])
+{
+	setlocale(LC_ALL, "");
 
-  if (text_len != 0) {
-    // guess language based on the first character
-    // don't try this at home
-    unsigned char c = (unsigned char)text[0];
-    if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')) {
-      memcpy(language_code, "en", 3);
-      return 0;
-    }
-
-    unsigned char next_c = (unsigned char)text[1];
-    if ((c == 0xD0 && (next_c == 0x81 || next_c >= 0x90)) || (c == 0xD1 && next_c <= 0x91)) {
-      memcpy(language_code, "ru", 3);
-      return 0;
-    }
-  }
-
-  memcpy(language_code, "other", 6);
-  return 0;
+	TelegramChannel telegramChannel(channel_info->title, channel_info->description, channel_info->post_count, channel_info->posts);
+	wstring text = telegramChannel.get_all_text();
+	pair<string, double> lang_score = detect_language(fastText, text);
+    memcpy(language_code, lang_score.first.c_str(), lang_score.first.size() + 1);
+    return 0;
 }
 
-int tgcat_detect_category(const struct TelegramChannelInfo *channel_info,
-                          double category_probability[TGCAT_CATEGORY_OTHER + 1]) {
-  (void)channel_info;
-  memset(category_probability, 0, sizeof(double) * (TGCAT_CATEGORY_OTHER + 1));
+int tgcat_detect_category(const struct TelegramChannelInfo *channel_info, double category_probability[TGCAT_CATEGORY_OTHER + 1])
+{
+    (void)channel_info;
+    memset(category_probability, 0, sizeof(double) * (TGCAT_CATEGORY_OTHER + 1));
+    int i;
 
-  int i;
-  for (i = 0; i < 10; i++) {
-    category_probability[rand() % (TGCAT_CATEGORY_OTHER + 1)] += 0.1;
-  }
-  return 0;
+    for (i = 0; i < 10; i++)
+    {
+        category_probability[rand() % (TGCAT_CATEGORY_OTHER + 1)] += 0.1;
+    }
+
+    return 0;
 }
---
+++ libtgcat/utils.h
@@ -0,0 +1,69 @@
+#include <vector>
+#include <locale>
+#include <cctype>
+#include <codecvt>
+
+#ifndef UTILS_H
+#define UTILS_H
+
+using namespace std;
+
+wstring char_to_wstring(const char* s)
+{
+    wstring_convert<codecvt_utf8<wchar_t>, wchar_t> converter;
+    return converter.from_bytes(s);
+}
+
+wstring utf8_to_wstring(const string& str)
+{
+    wstring_convert<codecvt_utf8<wchar_t>> myconv;
+    return myconv.from_bytes(str);
+}
+
+string wstring_to_utf8(const wstring& str)
+{
+    wstring_convert<codecvt_utf8<wchar_t>> myconv;
+    return myconv.to_bytes(str);
+}
+
+struct TelegramChannel
+{
+    wstring title;
+    wstring description;
+    vector<wstring> posts;
+
+    TelegramChannel(wstring title, wstring description, vector<wstring> posts)
+    {
+        this->title = title;
+        this->description = description;
+        this->posts = posts;
+    }
+
+    TelegramChannel(const char* title, const char* description, size_t post_count, const char** posts)
+    {
+        this->title = char_to_wstring(title);
+        this->description = char_to_wstring(description);
+        this->posts = vector<wstring>();
+
+        for (size_t i = 0; i < post_count; i++)
+        {
+            this->posts.push_back(char_to_wstring(posts[i]));
+        }
+    }
+
+    wstring get_all_text()
+    {
+        wstring result;
+        result += this->title + L"\n";
+        result += this->description + L"\n";
+
+        for (size_t i = 0, post_count = posts.size(); i < post_count; i++)
+        {
+            result += this->posts[i] + L"\n";
+        }
+
+        return result;
+    }
+};
+
+#endif
